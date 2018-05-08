4948dd806fd3abf583ee64cee031b184aea20d11
Karolina Madej <kkarolinamadej@gmail.com>
2018-05-08 19:59:24 +0200

    Add plugin, add carousel

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  pageDots: false,
+  hash: true
+});
+
+
+var restartButton = document.querySelector('.button--restart');
+restartButton.addEventListener('click', function() {
+  flkty.select( 0);
+});
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
