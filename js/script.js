708725a074caedf424a69afe07be2d88cb1af43d
Karolina Madej <kkarolinamadej@gmail.com>
2018-05-09 14:40:52 +0200

    Add Mustache template

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+var templateItem = document.getElementById('template-slide-item').innerHTML;
+Mustache.parse(templateItem);
+var listItems = '';
+
+for(var i = 0; i < slides.length; i++){
+  //console.log(productsData);
+  listItems += Mustache.render(templateItem, slides[i]);
+};
+
+carousel.insertAdjacentHTML('beforeend', listItems);
+
+
 
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
