67e96003ecfa732f62fc59dd8add896328c389d6
Karolina Madej <kkarolinamadej@gmail.com>
2018-05-10 17:22:28 +0200

    Link carousuel and map

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Mustache html template
+
 var templateItem = document.getElementById('template-slide-item').innerHTML;
 Mustache.parse(templateItem);
 var listItems = '';
@@ -9,7 +11,7 @@ for(var i = 0; i < slides.length; i++){
 
 carousel.insertAdjacentHTML('beforeend', listItems);
 
-
+// Flickity plugin
 
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
@@ -20,10 +22,9 @@ var flkty = new Flickity( elem, {
   hash: true
 });
 
-
 var restartButton = document.querySelector('.button--restart');
 restartButton.addEventListener('click', function() {
-  flkty.select( 0);
+  flkty.select(0);
 });
 
 var progressBar = document.querySelector('.progress-bar')
@@ -34,15 +35,34 @@ flkty.on( 'scroll', function( progress ) {
 });
 
 
+// Google maps + flickity
+
+var par = true;
+
 window.initMap = function() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 3,
-    center: slides[0].coords
+    zoom: 10,
+    center: slides[flkty.selectedIndex].coords
   });
   for(var i = 0; i < slides.length; i++) {
     var marker = new google.maps.Marker({
       position: slides[i].coords,
       map: map
     });
-  }
+    (function(i){
+      marker.addListener('click', function() {
+        par = false;
+        flkty.select(i);
+      });
+    })(i);
+  };
+
+  flkty.on( 'change', function( index ) {
+    if (par) {
+      map.panTo(slides[index].coords);
+      map.setZoom(10);
+    } else {
+      par = true;
+    };
+  });
 };
